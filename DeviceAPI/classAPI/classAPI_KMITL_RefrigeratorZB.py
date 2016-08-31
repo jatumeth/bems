2a116f7d1d1f28d3517b3f8ccab21b095908ff4f
enerphantdev <kenshero.k@gmail.com>
2016-08-31 23:24:41 +0700

    update Refrig

--- DeviceAPI/classAPI/classAPI_KMITL_RefrigeratorZB.py
+++ DeviceAPI/classAPI/classAPI_KMITL_RefrigeratorZB.py
@@ -9,16 +9,11 @@ class API:
     def __init__(self,**kwargs):  # default color is white
         # Initialized common attributes
         self.variables = kwargs
-        self.debug = True
-        self.set_variable('offline_count',0)
-        self.set_variable('connection_renew_interval',6000) #nothing to renew, right now
-        self.only_white_bulb = None
-        # to initialize the only white bulb value
-        self.set_variable('_username', "test")
-        self.set_variable('_api_key', "576ce7157410fef051b42ed5ed393498dc58a1b5")
-        self.set_variable('_address', "http://192.168.1.13")
-        self.set_variable('_id', "15")
-        #self.getDeviceStatus()
+        self._username = "test"
+        self._api_key = "576ce7157410fef051b42ed5ed393498dc58a1b5"
+        self._address = "http://192.168.1.13"
+        self._id = "15"
+        self.getDeviceStatus()
 
 
     def renewConnection(self):
@@ -34,10 +29,8 @@ class API:
     '''
     Attributes:
      ------------------------------------------------------------------------------------------
-    status            GET    POST      Philips Hue ON/OFF status
-    brightness        GET    POST      brightness percentage
-    effect            GET    POST      Hue light effect 'none' or 'colorloop'
-    color             GET    POST      temporary target heat setpoint (floating point in deg F)
+    current_temperature     GET    POST      Refrigerator ON/OFF status
+    power                   GET    POST      Power
      ------------------------------------------------------------------------------------------
 
     '''
@@ -47,38 +40,28 @@ class API:
     API3 available methods:
     1. getDeviceStatus() GET
     2. setDeviceStatus(postmsg) PUT
-    3. identifyDevice()
     '''
 
     # ----------------------------------------------------------------------
     # getDeviceStatus(), getDeviceStatusJson(data), printDeviceStatus()
     def getDeviceStatus(self):
 
-        _url_append_get =  self.get_variable('_address')+ '/api/appliances/'+ self.get_variable('_id')+'/info/?format=json&username=test&api_key='+ self.get_variable('_api_key')
-        r = requests.get(_url_append_get)
-        print _url_append_get
+        _url_append =  self._address + '/api/appliances/'+ self._id+'/info/?format=json&username=test&api_key='+ self._api_key
+        r = requests.get(_url_append)
+        #print _url_append
         _theJSON = json.loads(r.content)
         #print _theJSON
         self.set_variable('current_temperature', _theJSON[0]["value"])
         self.set_variable('power', _theJSON[1]["value"])
 
-
-
-
         print(" Current_temperature = {}".format(self.get_variable('current_temperature')))
         print(" Power = {}".format(self.get_variable('power')))
 
 
-
-
-
     def setDeviceStatus(self, postmsg):
-        _url_append = self.get_variable('_address') + '/api/appliances/' + self.get_variable(
-            '_id') + '/command/?username=' + self.get_variable('_username') + '&api_key=' + self.get_variable(
-            '_api_key')
+        _url_append = self._address + '/api/appliances/' + self._id + '/command/?username=' + self._username + '&api_key=' + self._api_key
         _body = []
         _data = self.convertPostMsg(postmsg)
-       # _data = _data.encode(encoding='utf_8')
         print _data
         for k in _data:
             print k
@@ -107,8 +90,8 @@ def main():
     # create an object with initialized data from DeviceDiscovery Agent
     # requirements for instantiation1. model, 2.type, 3.api, 4. address
     RefrigeratorZB = API(model='Philips Hue',type='wifiLight',api='API3',address='http://192.168.1.13',username='acquired username',agent_id='LightingAgent')
-    #RefrigeratorZB.getDeviceStatus()
-    RefrigeratorZB.setDeviceStatus({"temp":"-5", "motor_speed": "128"})
+    RefrigeratorZB.getDeviceStatus()
+    #RefrigeratorZB.setDeviceStatus({"temp":"-5", "motor_speed": "128"})
     #RefrigeratorZB.identifyDevice()
 
 
