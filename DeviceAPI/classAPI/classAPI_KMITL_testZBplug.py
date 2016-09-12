85057b58039a528955211f7acd4e466f7e12a31b
enerphantdev <kenshero.k@gmail.com>
2016-09-12 22:26:59 +0700

    Update ClassAPI ZBpulg and add_ClassAPI SmartMeter

--- DeviceAPI/classAPI/classAPI_KMITL_testZBplug.py
+++ DeviceAPI/classAPI/classAPI_KMITL_testZBplug.py
@@ -1,8 +1,8 @@
 import time
 import urllib2
 import json
-
 import requests
+
 class API:
     # 1. constructor : gets call every time when create a new class
     # requirements for instantiation1. model, 2.type, 3.api, 4. address
@@ -15,8 +15,14 @@ class API:
         self._username = "test"
         self._api_key = "576ce7157410fef051b42ed5ed393498dc58a1b5"
         self._address = "http://192.168.1.13"
-        self._id = "14"
-        self.getDeviceStatus()
+        self._id_Smartplug_0 = "14"
+        self._id_Smartplug_1 = "19"
+        self._id_Smartplug_2 = "20"
+        self._id_Smartplug_3 = "21"
+        self._id_Smartplug_4 = "22"
+        self._id_Smartplug_5 = "23"
+        self._id_Smartplug_6 = "24"
+
     def renewConnection(self):
         pass
 
@@ -45,7 +51,24 @@ class API:
 
     # ----------------------------------------------------------------------
     # getDeviceStatus(), getDeviceStatusJson(data), printDeviceStatus()
-    def getDeviceStatus(self):
+    def getDeviceStatus(self, postmsg):
+
+        for k in postmsg:
+            if k == 'Smartplug_0':
+                self._id = self._id_Smartplug_0
+            elif k == 'Smartplug_1':
+                self._id = self._id_Smartplug_1
+            elif k == 'Smartplug_2':
+                self._id = self._id_Smartplug_2
+            elif k == 'Smartplug_3':
+                self._id = self._id_Smartplug_3
+            elif k == 'Smartplug_4':
+                self._id = self._id_Smartplug_4
+            elif k == 'Smartplug_5':
+                self._id = self._id_Smartplug_5
+            elif k == 'Smartplug_6':
+                self._id = self._id_Smartplug_6
+
         _url_append = self._address + '/api/appliances/' + self._id +'/info/?format=json&username=' + self._username + '&api_key=' + self._api_key
         #print _url_append
         r = requests.get(_url_append)
@@ -55,20 +78,39 @@ class API:
         self.set_variable('volt', _theJSON[1]["value"])
         self.set_variable('current', _theJSON[2]["value"])
         self.set_variable('power', _theJSON[3]["value"])
-        self.set_variable('current_consumption', _theJSON[1]["value"])
+        self.set_variable('energy_kWh', _theJSON[4]["value"])
+        self.set_variable('PF', _theJSON[5]["value"])
 
         print(" status = {}".format(self.get_variable('status')))
         print(" volt = {}".format(self.get_variable('volt')))
         print(" current = {}".format(self.get_variable('current')))
         print(" power = {}".format(self.get_variable('power')))
-        print(" current_consumption = {}".format(self.get_variable('current_consumption')))
-
+        print(" Energy_kWh = {}".format(self.get_variable('energy_kwh')))
+        print(" PF = {}".format(self.get_variable('PF')))
 
     def setDeviceStatus(self, postmsg):
+
+        for k in postmsg :
+            if k == 'Smartplug_0':
+                self._id = self._id_Smartplug_0
+            elif k == 'Smartplug_1':
+                self._id = self._id_Smartplug_1
+            elif k == 'Smartplug_2':
+                self._id = self._id_Smartplug_2
+            elif k == 'Smartplug_3':
+                self._id = self._id_Smartplug_3
+            elif k == 'Smartplug_4':
+                self._id = self._id_Smartplug_4
+            elif k == 'Smartplug_5':
+                self._id = self._id_Smartplug_5
+            elif k == 'Smartplug_6':
+                self._id = self._id_Smartplug_6
+
         if postmsg.get('status') == "ON":
             self._set_On_Off_parameters = 1
         elif postmsg.get('status') == "OFF":
             self._set_On_Off_parameters =  0
+
 
         self._body_On_Off = {"cmd_name" : "set_On_Off" , "parameters" : {"setTo" : self._set_On_Off_parameters}}
         self._url_append = self._address + '/api/appliances/' + self._id + '/command/?username='+ self._username+'&api_key='+ self._api_key
@@ -76,13 +118,79 @@ class API:
         #print self._body_On_Off
         r = requests.post(self._url_append,data = json.dumps(self._body_On_Off))
 
+    def identifyDevice(self, postmsg):
+
+
+        for k in postmsg :
+            if k == 'Smartplug_0':
+                self._id = self._id_Smartplug_0
+                self.getDeviceStatus({"Smartplug_0": "0"})
+            elif k == 'Smartplug_1':
+                self._id = self._id_Smartplug_1
+                self.getDeviceStatus({"Smartplug_1": "0"})
+            elif k == 'Smartplug_2':
+                self._id = self._id_Smartplug_2
+                self.getDeviceStatus({"Smartplug_2": "0"})
+            elif k == 'Smartplug_3':
+                self._id = self._id_Smartplug_3
+                self.getDeviceStatus({"Smartplug_3": "0"})
+            elif k == 'Smartplug_4':
+                self._id = self._id_Smartplug_4
+                self.getDeviceStatus({"Smartplug_4": "0"})
+            elif k == 'Smartplug_5':
+                self._id = self._id_Smartplug_5
+                self.getDeviceStatus({"Smartplug_5": "0"})
+            elif k == 'Smartplug_6':
+                self._id = self._id_Smartplug_6
+                self.getDeviceStatus({"Smartplug_6": "0"})
+
+        self._body_OFF = {"cmd_name": "set_On_Off", "parameters": {"setTo": 0}}
+        self._body_ON = {"cmd_name": "set_On_Off", "parameters": {"setTo": 1}}
+        self._url_append = self._address + '/api/appliances/' + self._id + '/command/?username=' + self._username + '&api_key=' + self._api_key
+
+        if self.get_variable('status') == 1 :
+            self.timeDelay(3)
+            r = requests.post(self._url_append, data = json.dumps(self._body_OFF))
+            print r
+            self.timeDelay(3)
+            r = requests.post(self._url_append, data = json.dumps(self._body_ON))
+            print r
+            self.timeDelay(3)
+            r = requests.post(self._url_append, data = json.dumps(self._body_OFF))
+            print r
+            self.timeDelay(3)
+            r = requests.post(self._url_append, data = json.dumps(self._body_ON))
+            print r
+        elif self.get_variable('status') == 0 :
+            self.timeDelay(3)
+            r = requests.post(self._url_append, data = json.dumps(self._body_ON))
+            print r
+            self.timeDelay(3)
+            r = requests.post(self._url_append, data = json.dumps(self._body_OFF))
+            print r
+            self.timeDelay(3)
+            r = requests.post(self._url_append, data = json.dumps(self._body_ON))
+            print r
+            self.timeDelay(3)
+            r = requests.post(self._url_append, data = json.dumps(self._body_OFF))
+            print r
+
+    def timeDelay(self,time_iden):  # specify time_iden for how long to delay the process
+        t0 = time.time()
+        self.seconds = time_iden
+        while time.time() - t0 <= time_iden:
+            self.seconds = self.seconds - 1
+            print("wait: {} sec".format(self.seconds))
+            time.sleep(1)
+
 def main():
     # create an object with initialized data from DeviceDiscovery Agent
     # requirements for instantiation1. model, 2.type, 3.api, 4. address
-    testZBplug = API(model='Philips Hue',type='wifiLight',api='API3',address='http://192.168.1.13',username='acquired username',agent_id='LightingAgent')
-    testZBplug.getDeviceStatus()
-    #testZBplug.setDeviceStatus({"status":"OFF"})
+    testZBplug = API(model='ZBplug',type='wifiLight',api='API3',address='http://192.168.1.13',username='acquired username',agent_id='LightingAgent')
+    #testZBplug.getDeviceStatus({"Smartplug_0":"1"})
+    #testZBplug.setDeviceStatus({"Smartplug_0":"0"})
     #testZBplug.identifyDevice()
+    testZBplug.identifyDevice({"Smartplug_0":"0"})
 
 
 if __name__ == "__main__": main()
