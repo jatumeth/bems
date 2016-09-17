131ec05ee0fdc7629ea5a9b3e4c8052a2a94943c
kwarodom <kwarodom@vt.edu>
2016-09-17 13:05:14 +0700

    Add EV calculation function

--- Agents/PowermeterAppAgent/powermeterapp/agent.py
+++ Agents/PowermeterAppAgent/powermeterapp/agent.py
@@ -85,66 +85,94 @@ class PowermeterAppAgent(PublishMixin, BaseAgent):
             energy_kWh = 0
         return energy_kWh
 
+    def EV_energy_calculate(self):
+        EVdata = {}
+        time_now = datetime.datetime.now()
+        weekday = time_now.weekday()
+        end_time = time_now
+        start_time = end_time.replace(hour=0, minute=0, second=0)
+        if (weekday == 5) | (weekday == 6) | (weekday == 7):  # provide "weekday = 7" for holiday rate
+            EVdata['daily_EV_energy'] = self.integrate_power('3WIS221445K1200321', 'power', start_time, end_time)
+            EVdata['daily_EV_bill'] = EVdata['daily_EV_energy'] * OFFPEAK_RATE
+        else:
+            # Peak Period --- 09:00 - 22:00
+            end_time = time_now.replace(hour=22, minute=0, second=0)
+            start_time = time_now.replace(hour=9, minute=0, second=1)
+            EVdata['daily_EV_energy_peak'] = self.integrate_power('3WIS221445K1200321', 'power', start_time, end_time)
+            EVdata['daily_EV_bill_peak'] = EVdata['daily_EV_energy_peak'] * PEAK_RATE
+
+            # Off Peak Period 1  --- 0:00 - 09:00
+            end_time = time_now.replace(hour=9, minute=0, second=0)
+            start_time = time_now.replace(hour=0, minute=0, second=0)
+            EVdata['daily_energy_offpeak1'] = self.integrate_power('3WIS221445K1200321', 'power', start_time, end_time)
+            EVdata['daily_EV_bill_offpeak1'] = EVdata['daily_EV_energy_offpeak1'] * OFFPEAK_RATE
+
+            # Off Peak Period 2 --- 22:00 - 23:59
+            end_time = time_now.replace(hour=23, minute=59, second=59)
+            start_time = time_now.replace(hour=22, minute=0, second=1)
+            EVdata['daily_energy_offpeak2'] = self.integrate_power('3WIS221445K1200321', 'power', start_time, end_time)
+            EVdata['daily_EV_bill_offpeak2'] = EVdata['daily_EV_energy_offpeak2'] * OFFPEAK_RATE
+
+            # sum all period
+            EVdata['daily_EV_energy'] = EVdata['daily_EV_energy_peak'] + EVdata['daily_EV_energy_offpeak1'] + EVdata[
+                'daily_EV_energy_offpeak2']
+            EVdata['daily_EV_bill'] = EVdata['daily_EV_bill_peak'] + EVdata['daily_EV_bill_offpeak1'] + EVdata[
+                'daily_EV_bill_offpeak2']
+        return EVdata
+
     def daily_energy_calculate(self):
         data = {}
         time_now = datetime.datetime.now()
         weekday = time_now.weekday()
         end_time = time_now
         start_time = end_time.replace(hour=0, minute=0, second=0)
+        EV_data = self.EV_energy_calculate()
         data['solar_daily_energy'] = self.integrate_power('SmappeePowerMeter', 'solar_activepower', start_time, end_time)
         if (weekday == 5)|(weekday == 6)|(weekday == 7):  #provide "weekday = 7" for holiday rate
             data['daily_energy'] = self.integrate_power('SmappeePowerMeter', 'load_activepower', start_time, end_time)
             data['daily_bill'] = data['daily_energy'] * OFFPEAK_RATE
-            data['daily_light_energy'] = data['daily_energy'] * 0.01
+            data['daily_light_energy'] = data['daily_energy'] * 0.1
             data['daily_light_bill'] = data['daily_light_energy'] * OFFPEAK_RATE
             data['daily_AC_energy'] = data['daily_energy'] * 0.5
-            data['daily_AC_bill'] = data['daily_light_energy'] * OFFPEAK_RATE
-            data['daily_plug_energy'] = data['daily_energy'] * 0.3
-            data['daily_plug_bill'] = data['daily_light_energy'] * OFFPEAK_RATE
-            data['daily_EV_energy'] = data['daily_energy'] * 0.19
-            data['daily_EV_bill'] = data['daily_light_energy'] * OFFPEAK_RATE
+            data['daily_AC_bill'] = data['daily_AC_energy'] * OFFPEAK_RATE
+            data['daily_plug_energy'] = data['daily_energy'] - (data['daily_AC_energy'] + data['daily_light_energy'] + EV_data['daily_EV_energy'])
+            data['daily_plug_bill'] = data['daily_plug_energy'] * OFFPEAK_RATE
         else:
             #Peak Period --- 09:00 - 22:00
             end_time = time_now.replace(hour=22, minute=0, second=0)
             start_time = time_now.replace(hour=9, minute=0, second=1)
             data['daily_energy_peak'] = self.integrate_power('SmappeePowerMeter', 'load_activepower', start_time, end_time)
             data['daily_bill_peak'] = data['daily_energy_peak'] * PEAK_RATE
-            data['daily_light_energy_peak'] = data['daily_energy_peak'] * 0.01
+            data['daily_light_energy_peak'] = data['daily_energy_peak'] * 0.1
             data['daily_light_bill_peak'] = data['daily_light_energy_peak'] * PEAK_RATE
             data['daily_AC_energy_peak'] = data['daily_energy_peak'] * 0.5
             data['daily_AC_bill_peak'] = data['daily_AC_energy_peak'] * PEAK_RATE
-            data['daily_plug_energy_peak'] = data['daily_energy_peak'] * 0.3
+            data['daily_plug_energy_peak'] = data['daily_energy_peak'] - (data['daily_AC_energy_peak'] + data['daily_light_energy_peak'] + EV_data['daily_EV_energy_peak'])
             data['daily_plug_bill_peak'] = data['daily_plug_energy_peak'] * PEAK_RATE
-            data['daily_EV_energy_peak'] = data['daily_energy_peak'] * 0.5
-            data['daily_EV_bill_peak'] = data['daily_EV_energy_peak'] * PEAK_RATE
 
             #Off Peak Period 1  --- 0:00 - 09:00
             end_time = time_now.replace(hour=9, minute=0, second=0)
             start_time = time_now.replace(hour=0, minute=0, second=0)
             data['daily_energy_offpeak1'] = self.integrate_power('SmappeePowerMeter', 'load_activepower', start_time, end_time)
             data['daily_bill_offpeak1'] = data['daily_energy_offpeak1'] * OFFPEAK_RATE
-            data['daily_light_energy_offpeak1'] = data['daily_energy_offpeak1'] * 0.01
+            data['daily_light_energy_offpeak1'] = data['daily_energy_offpeak1'] * 0.1
             data['daily_light_bill_offpeak1'] = data['daily_light_energy_offpeak1'] * OFFPEAK_RATE
             data['daily_AC_energy_offpeak1'] = data['daily_energy_offpeak1'] * 0.5
             data['daily_AC_bill_offpeak1'] = data['daily_AC_energy_offpeak1'] * OFFPEAK_RATE
-            data['daily_plug_energy_offpeak1'] = data['daily_energy_offpeak1'] * 0.3
+            data['daily_plug_energy_offpeak1'] = data['daily_energy_offpeak1'] - (data['daily_AC_energy_offpeak1'] + data['daily_light_energy_offpeak1'] + EV_data['daily_EV_energy_offpeak1'])
             data['daily_plug_bill_offpeak1'] = data['daily_plug_energy_offpeak1'] * OFFPEAK_RATE
-            data['daily_EV_energy_offpeak1'] = data['daily_energy_offpeak1'] * 0.5
-            data['daily_EV_bill_offpeak1'] = data['daily_EV_energy_offpeak1'] * OFFPEAK_RATE
 
             #Off Peak Period 2 --- 22:00 - 23:59
             end_time = time_now.replace(hour=23, minute=59, second=59)
             start_time = time_now.replace(hour=22, minute=0, second=1)
             data['daily_energy_offpeak2'] = self.integrate_power('SmappeePowerMeter', 'load_activepower', start_time, end_time)
             data['daily_bill_offpeak2'] = data['daily_energy_offpeak2'] * OFFPEAK_RATE
-            data['daily_light_energy_offpeak2'] = data['daily_energy_offpeak2'] * 0.01
+            data['daily_light_energy_offpeak2'] = data['daily_energy_offpeak2'] * 0.1
             data['daily_light_bill_offpeak2'] = data['daily_light_energy_offpeak2'] * OFFPEAK_RATE
             data['daily_AC_energy_offpeak2'] = data['daily_energy_offpeak2'] * 0.5
             data['daily_AC_bill_offpeak2'] = data['daily_AC_energy_offpeak2'] * OFFPEAK_RATE
-            data['daily_plug_energy_offpeak2'] = data['daily_energy_offpeak2'] * 0.3
+            data['daily_plug_energy_offpeak2'] = data['daily_energy_offpeak2'] - (data['daily_AC_energy_offpeak2'] + data['daily_light_energy_offpeak2'] + EV_data['daily_EV_energy_offpeak2'])
             data['daily_plug_bill_offpeak2'] = data['daily_plug_energy_offpeak2'] * OFFPEAK_RATE
-            data['daily_EV_energy_offpeak2'] = data['daily_energy_offpeak2'] * 0.5
-            data['daily_EV_bill_offpeak2'] = data['daily_EV_energy_offpeak2'] * OFFPEAK_RATE
 
             #sum all period
             data['daily_energy'] = data['daily_energy_peak'] + data['daily_energy_offpeak1'] + data['daily_energy_offpeak2']
@@ -155,8 +183,6 @@ class PowermeterAppAgent(PublishMixin, BaseAgent):
             data['daily_AC_bill'] = data['daily_AC_bill_peak'] + data['daily_AC_bill_offpeak1'] + data['daily_AC_bill_offpeak2']
             data['daily_plug_energy'] = data['daily_plug_energy_peak'] + data['daily_plug_energy_offpeak1'] + data['daily_plug_energy_offpeak2']
             data['daily_plug_bill'] = data['daily_plug_bill_peak'] + data['daily_plug_bill_offpeak1'] + data['daily_plug_bill_offpeak2']
-            data['daily_EV_energy'] = data['daily_EV_energy_peak'] + data['daily_EV_energy_offpeak1'] + data['daily_EV_energy_offpeak2']
-            data['daily_EV_bill'] = data['daily_EV_bill_peak'] + data['daily_EV_bill_offpeak1'] + data['daily_EV_bill_offpeak2']
 
         #------Daily_Energy_Backup-------
         end_day_time = time_now.replace(hour=23, minute=59, second=59)
@@ -167,14 +193,13 @@ class PowermeterAppAgent(PublishMixin, BaseAgent):
                              'load_bill': data['daily_bill'], 'light_energy':data['daily_light_energy'],
                              'light_bill': data['daily_light_bill'], 'AC_energy': data['daily_AC_energy'],
                              'AC_bill': data['daily_AC_bill'], 'plug_energy': data['daily_plug_energy'],
-                             'plug_bill': data['daily_plug_bill'], 'EV_energy': data['daily_EV_energy'],
-                             'EV_bill': data['daily_EV_bill']}
+                             'plug_bill': data['daily_plug_bill'], 'EV_energy': EV_data['daily_EV_energy'],
+                             'EV_bill': EV_data['daily_EV_bill']}
                 cassandraDB.insert('DailyData', variables, log_variables)
                 print('Data Pushed to cassandra as a backup')
             except Exception as er:
                 print("ERROR: {} fails to update cassandra database".format('DailyData'))
                 print er
-
         return data
 
     def last_day_energy_calculate(self):
@@ -183,22 +208,22 @@ class PowermeterAppAgent(PublishMixin, BaseAgent):
         delta_time = datetime.timedelta(days=1)
         time = time_now - delta_time
         end_time = time.replace(hour=23, minute=59, second=59)
-        start_time = time.replace(hour=0, minute=0, second=0)
+        start_time = time.replace(hour=23, minute=59, second=50)
         try:
             data_points, rs = retrieve('DailyData', vars=['load_energy', 'solar_energy', 'load_bill', 'light_energy',
                                                       'light_bill', 'AC_energy', 'AC_bill', 'plug_energy', 'plug_bill',
                                                       'EV_energy', 'EV_bill'], startTime=start_time, endTime=end_time)
-            data['last_day_energy'] = rs[0]
-            data['solar_last_day_energy'] = rs[1]
-            data['last_day_bill'] = rs[2]
-            data['last_day_light_energy'] = rs[3]
-            data['last_day_light_bill'] = rs[4]
-            data['last_day_AC_energy'] = rs[5]
-            data['last_day_AC_bill'] = rs[6]
-            data['last_day_plug_energy'] = rs[7]
-            data['last_day_plug_bill'] = rs[8]
-            data['last_day_EV_energy'] = rs[9]
-            data['last_day_EV_bill'] = rs[10]
+            data['last_day_energy'] = rs[len(rs)-1][0]
+            data['solar_last_day_energy'] = rs[len(rs)-1][1]
+            data['last_day_bill'] = rs[len(rs)-1][2]
+            data['last_day_light_energy'] = rs[len(rs)-1][3]
+            data['last_day_light_bill'] = rs[len(rs)-1][4]
+            data['last_day_AC_energy'] = rs[len(rs)-1][5]
+            data['last_day_AC_bill'] = rs[len(rs)-1][6]
+            data['last_day_plug_energy'] = rs[len(rs)-1][7]
+            data['last_day_plug_bill'] = rs[len(rs)-1][8]
+            data['last_day_EV_energy'] = rs[len(rs)-1][9]
+            data['last_day_EV_bill'] = rs[len(rs)-1][10]
         except:
             data['last_day_energy'] = 0
             data['solar_day_energy'] = 0
@@ -211,7 +236,6 @@ class PowermeterAppAgent(PublishMixin, BaseAgent):
             data['last_day_plug_bill'] = 0
             data['last_day_EV_energy'] = 0
             data['last_day_EV_bill'] = 0
-
         return data
 
     def monthly_energy_usage_calculate(self):
@@ -280,6 +304,8 @@ class PowermeterAppAgent(PublishMixin, BaseAgent):
             'data_source': "powermeterApp",
         }
         daily_data = self.daily_energy_calculate()
+        EV_daily_data = self.EV_energy_calculate()
+
         if (daily_data['daily_energy'] < PowermeterAppAgent.check_daily_data['daily_energy']):
             daily_data['daily_energy'] = PowermeterAppAgent.check_daily_data['daily_energy']
 
@@ -295,15 +321,16 @@ class PowermeterAppAgent(PublishMixin, BaseAgent):
         if (daily_data['daily_plug_bill'] < PowermeterAppAgent.check_daily_data['daily_plug_bill']):
             daily_data['daily_plug_bill'] = PowermeterAppAgent.check_daily_data['daily_plug_bill']
 
-        if (daily_data['daily_EV_bill'] < PowermeterAppAgent.check_daily_data['daily_EV_bill']):
-            daily_data['daily_EV_bill'] = PowermeterAppAgent.check_daily_data['daily_EV_bill']
+        if (EV_daily_data['daily_EV_bill'] < PowermeterAppAgent.check_daily_data['daily_EV_bill']):
+            EV_daily_data['daily_EV_bill'] = PowermeterAppAgent.check_daily_data['daily_EV_bill']
+
 
         daily_energy_usage = round(daily_data['daily_energy'], 2)
         daily_electricity_bill = round(daily_data['daily_bill'], 2)
         daily_bill_light = round(daily_data['daily_light_bill'], 2)
         daily_bill_AC = round(daily_data['daily_AC_bill'], 2)
         daily_bill_plug = round(daily_data['daily_plug_bill'], 2)
-        daily_bill_EV = round(daily_data['daily_EV_bill'], 2)
+        daily_bill_EV = round(EV_daily_data['daily_EV_bill'], 2)
 
         last_day_data = self.last_day_energy_calculate()
         last_day_energy_usage = round(last_day_data['last_day_energy'], 2)
@@ -410,6 +437,8 @@ class PowermeterAppAgent(PublishMixin, BaseAgent):
                               "netzero_energy_consumption": netzero_energy_consumption})
         self.publish(topic, headers, message)
         PowermeterAppAgent.check_daily_data = daily_data
+        PowermeterAppAgent.check_daily_data['daily_EV_bill'] = EV_daily_data['daily_EV_bill']
+        PowermeterAppAgent.check_daily_data['daily_EV_energy'] = EV_daily_data['daily_EV_energy']
         print ("{} published topic: {}, message: {}").format(self._agent_id, topic, message)
 
 def main(argv=sys.argv):
