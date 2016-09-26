ca7052542b86e30f1e3542ed642858aa177666bd
kwarodom <kwarodom@vt.edu>
2016-09-27 01:48:20 +0700

    Edit Header

--- Agents/PowermeterAppAgent2/powermeterapp/agent.py
+++ Agents/PowermeterAppAgent2/powermeterapp/agent.py
@@ -33,15 +33,7 @@ class PowermeterAppAgent(PublishMixin, BaseAgent):
     '''Listens to everything and publishes a heartbeat according to the
     heartbeat period specified in the settings module.
     '''
-    check_daily_data = {'daily_energy': 0.0, 'daily_bill': 0.0, 'daily_light_bill': 0.0, 'daily_AC_bill': 0.0,
-                        'daily_plug_bill': 0.0, 'daily_EV_bill': 0.0, 'solar_energy': 0.0}
 
-    check_last_day_data = {'daily_energy': 0.0, 'daily_bill': 0.0, 'daily_light_bill': 0.0, 'daily_AC_bill': 0.0,
-                        'daily_plug_bill': 0.0, 'daily_EV_bill': 0.0, 'solar_energy': 0.0}
-
-    flag_last_month = False
-    flag_this_month = False
-    daily_bill_EV = 0
 
     def __init__(self, config_path, **kwargs):
         super(PowermeterAppAgent, self).__init__(**kwargs)
@@ -54,178 +46,95 @@ class PowermeterAppAgent(PublishMixin, BaseAgent):
         # Always call the base class setup()
         super(PowermeterAppAgent, self).setup()
 
-    # @matching.match_all
-    # def on_match(self, topic, headers, message, match):
-    #     '''Use match_all to receive all messages and print them out.'''
-    #     _log.debug("Topic: {topic}, Headers: {headers}, "
-    #                      "Message: {message}".format(
-    #                      topic=topic, headers=headers, message=message))
-    #     print("")
-
-    @matching.match_start("/agent/ui/dashboard/EVApp")
-    def on_match(self, topic, headers, message, match):
+    @matching.match_exact('/agent/ui/power_meter/device_status_response/bemoss/999/SmappeePowerMeter')
+    def on_match_smappee(self, topic, headers, message, match):
         '''Use match_all to receive all messages and print them out.'''
         _log.debug("Topic: {topic}, Headers: {headers}, "
                          "Message: {message}".format(
                          topic=topic, headers=headers, message=message))
-        print "EVApp Topic: {}".format(topic)
-        print "EVApp Headers: {}".format(headers)
-        print "EVApp Message: {}".format(message)
-        print "***************************************************"
-        print "***************************************************"
-        print "***************************************************"
-        print "***************************************************"
-        print "***************************************************"
-        print "***************************************************"
-        print "***************************************************"
-        self.message_from_EV = json.loads(message[0])
-        # print self.message_from_EV
+        message_from_Smappee = json.loads(message[0])
+        self.power_from_load = message_from_Smappee['load_activePower']
+        self.power_from_solar = message_from_Smappee['solar_activePower']
+        self.power_from_grid = message_from_Smappee['grid_activePower']
+
+    @matching.match_start('/agent/ui/')
+    def on_match(self, topic, headers, message, match):
+        '''Use match_all to receive all messages and print them out.'''
+        _log.debug("Topic: {topic}, Headers: {headers}, "
+                   "Message: {message}".format(
+            topic=topic, headers=headers, message=message))
+        dict_headers = dict(headers)
+        try:
+            data_source = dict_headers['data_source']
+            if (data_source == "realtime"):
+                agent_id = dict_headers['agent_id']
+                print agent_id
+        except:
+            print "No header that interesting"
+
+
+    # / agent / ui / power_meter / device_status_response / bemoss / 999 / SmappeePowerMeter
+    # ['{"grid_phasediff": 0.0, "solar_reactivePower": 98.912, "load_phasediff": 0.0, "db_password": "admin",
+    # "solar_quadrant": 0.0, "api": "classAPI_PowerMeter", "addressl": "http://Smappee1006003343.local/gateway/apipublic/logon",
+    # "connection_renew_interval": 6000, "volttage": 229.2, "load_apparentPower": 1604.061, "usernameq": {"Authorization": "admin"},
+    # "grid_powerfactor": 0.77, "grid_current": 6.995, "solar_activePower": 15.358, "solar_phasediff": 0.0, "offline_count": 0,
+    # "grid_quadrant": 0.0, "grid_phaseshift": 0.0, "addressq": "http://Smappee1006003343.local/gateway/apipublic/reportInstantaneousValues",
+    # "type": "power_meter", "usernamel": "admin", "grid_activePower": 1247.172, "load_reactivePower": 1008.748, "load_quadrant": 0.0,
+    # "load_current": 6.995, "db_port": "5432", "solar_phaseshift": 0.0, "grid_reactivePower": 1008.748, "agent_id": "SmappeePowerMeter",
+    # "load_powerfactor": 0.77, "grid_apparentPower": 1604.061, "solar_apparentPower": 100.098, "load_activePower": 1247.172,
+    # "db_database": "bemossdb", "load_phaseshift": 0.0, "solar_current": 0.436, "db_user": "admin",
+    # "db_host": "localhost", "time": 1474905838.075941, "model": "Smappee", "solar_powerfactor": 0.14}']
 
     # Demonstrate periodic decorator and settings access
-    @periodic(publish_periodic)
-
-    def publish_heartbeat(self):
-        '''Send heartbeat message every HEARTBEAT_PERIOD seconds.
-
-        HEARTBEAT_PERIOD is set and can be adjusted in the settings module.
-        '''
-        topic = "/agent/ui/dashboard"
-        now = datetime.datetime.utcnow().isoformat(' ') + 'Z'
-        headers = {
-            'AgentID': self._agent_id,
-            headers_mod.CONTENT_TYPE: headers_mod.CONTENT_TYPE.PLAIN_TEXT,
-            headers_mod.DATE: now,
-            'data_source': "powermeterApp",
-        }
-        time_now = datetime.datetime.now()
-        delta_time = datetime.timedelta(days=1)
-        timedelta = time_now - delta_time
-        last_day = timedelta.replace(hour=23, minute=59, second=59)
-
-        daily_data = day_energy_bill_calculation(self.check_daily_data, time_now)
-        last_day_data = last_day_usage(last_day)
-
-        daily_energy_usage = round(daily_data['daily_energy'], 2)
-        daily_electricity_bill = round(daily_data['daily_bill'], 2)
-        daily_bill_light = round(daily_data['daily_light_bill'], 2)
-        daily_bill_AC = round(daily_data['daily_AC_bill'], 2)
-        daily_bill_plug = round(daily_data['daily_plug_bill'], 2)
-        # daily_bill_EV = round(daily_data['daily_EV_bill'], 2)
-        try:
-            daily_bill_EV = round(self.message_from_EV["EV_bill"], 2)
-        except:
-            daily_bill_EV = 0
-        last_day_energy_usage = round(last_day_data['last_day_energy'], 2)
-        last_day_bill = round(last_day_data['last_day_bill'], 2)
-        last_day_bill_compare = round(daily_electricity_bill - last_day_bill, 2)
-        last_day_bill_light = round(last_day_data['last_day_light_bill'], 2)
-        last_day_bill_AC = round(last_day_data['last_day_AC_energy'], 2)
-        last_day_bill_plug = round(last_day_data['last_day_plug_bill'], 2)
-        last_day_bill_EV = round(last_day_data['last_day_EV_bill'], 2)
-
-        monthly_data = monthly_energy_bill_calculation("Current")
-        last_month_data = monthly_energy_bill_calculation("Last")
-
-        monthly_energy_usage = round(monthly_data['month_energy'], 2)
-        last_month_energy_usage = round(last_month_data['month_energy'], 2)
-        monthly_electricity_bill = round(monthly_data['month_bill'], 2)
-        last_month_bill = round(last_month_data['month_bill'], 2)
-        last_month_bill_compare = round(monthly_electricity_bill-last_month_bill, 2)
-
-        try:
-            daily_bill_AC_compare_percent = round(((daily_bill_AC - last_day_bill_AC) / last_day_bill_AC * 100), 2)
-        except:
-            daily_bill_AC_compare_percent = 100
-        try:
-            daily_bill_light_compare_percent = round(((daily_bill_light - last_day_bill_light) / last_day_bill_light * 100), 2)
-        except:
-            daily_bill_light_compare_percent = 100
-        try:
-            daily_bill_plug_compare_percent = round(((daily_bill_plug - last_day_bill_plug) / last_day_bill_plug * 100), 2)
-        except:
-            daily_bill_plug_compare_percent = 100
-        try:
-            daily_bill_EV_compare_percent = round(((daily_bill_EV - last_day_bill_EV) / last_day_bill_EV * 100), 2)
-        except:
-            daily_bill_EV_compare_percent = 100
-
-        monthly_bill_AC = round(monthly_data['month_AC_bill'], 2)
-        monthly_bill_light = round(monthly_data['month_light_bill'], 2)
-        monthly_bill_plug = round(monthly_data['month_plug_bill'], 2)
-        monthly_bill_EV = round(monthly_data['month_EV_bill'], 2)
-
-        last_month_bill_AC = round(last_month_data['month_AC_bill'], 2)
-        last_month_bill_light = round(last_month_data['month_AC_bill'], 2)
-        last_month_bill_plug = round(last_month_data['month_plug_bill'], 2)
-        last_month_bill_EV = round(last_month_data['month_EV_bill'], 2)
-
-        try:
-            monthly_bill_AC_compare_percent = round(((monthly_bill_AC - last_month_bill_AC) / last_month_bill_AC * 100), 2)
-        except:
-            monthly_bill_AC_compare_percent = 100
-        try:
-            monthly_bill_light_compare_percent = round(((monthly_bill_light - last_month_bill_light) / last_month_bill_light * 100), 2)
-        except:
-            monthly_bill_light_compare_percent = 100
-        try:
-            monthly_bill_plug_compare_percent = round(((monthly_bill_plug - last_month_bill_plug) / last_month_bill_plug * 100), 2)
-        except:
-            monthly_bill_plug_compare_percent = 100
-        try:
-            monthly_bill_EV_compare_percent = round(((monthly_bill_EV - last_month_bill_EV) / last_month_bill_EV * 100), 2)
-        except:
-            monthly_bill_EV_compare_percent = 100
-
-        max_monthly_energy_usage = 300
-        month = ["Jan", "Feb", "Mar", "April", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-        max_energy_usage_month = month[random.randint(0, 11)]
-        max_energy_usage_year = "2016"
-
-        annual_load_energy, annual_solar_energy = annual_energy_calculate()
-        netzero_onsite_generation = round(annual_solar_energy, 2)
-        netzero_energy_consumption = round(annual_load_energy, 2)
-
-        if (netzero_energy_consumption > netzero_onsite_generation):
-            netzero_condition = False
-        else:
-            netzero_condition = True
-
-        message = json.dumps({"monthly_electricity_bill": monthly_electricity_bill,
-                              "last_month_bill": last_month_bill,
-                              "last_month_bill_compare": last_month_bill_compare,
-                              "daily_electricity_bill": daily_electricity_bill,
-                              "last_day_bill": last_day_bill,
-                              "last_day_bill_compare": last_day_bill_compare,
-                              "daily_bill_AC": daily_bill_AC,
-                              "daily_bill_light": daily_bill_light,
-                              "daily_bill_plug": daily_bill_plug,
-                              "daily_bill_EV": daily_bill_EV,
-                              "daily_bill_AC_compare_percent": daily_bill_AC_compare_percent,
-                              "daily_bill_light_compare_percent": daily_bill_light_compare_percent,
-                              "daily_bill_plug_compare_percent": daily_bill_plug_compare_percent,
-                              "daily_bill_EV_compare_percent": daily_bill_EV_compare_percent,
-                              "monthly_bill_AC": monthly_bill_AC,
-                              "monthly_bill_light": monthly_bill_light,
-                              "monthly_bill_plug": monthly_bill_plug,
-                              "monthly_bill_EV": daily_bill_EV, #need to fix it
-                              "monthly_bill_AC_compare_percent": monthly_bill_AC_compare_percent,
-                              "monthly_bill_light_compare_percent": monthly_bill_light_compare_percent,
-                              "monthly_bill_plug_compare_percent": monthly_bill_plug_compare_percent,
-                              "monthly_bill_EV_compare_percent": monthly_bill_EV_compare_percent,
-                              "daily_energy_usage": daily_energy_usage,
-                              "last_day_energy_usage": last_day_energy_usage,
-                              "monthly_energy_usage": monthly_energy_usage,
-                              "last_month_energy_usage": last_month_energy_usage,
-                              "max_monthly_energy_usage": max_monthly_energy_usage,
-                              "max_energy_usage_month": max_energy_usage_month,
-                              "max_energy_usage_year": max_energy_usage_year,
-                              "netzero_condition": netzero_condition,
-                              "netzero_onsite_generation": netzero_onsite_generation,
-                              "netzero_energy_consumption": netzero_energy_consumption})
-        self.publish(topic, headers, message)
-        PowermeterAppAgent.check_daily_data = daily_data
-        PowermeterAppAgent.check_last_day_data = last_day_data
-        print ("{} published topic: {}, message: {}").format(self._agent_id, topic, message)
+    # @periodic(publish_periodic)
+    #
+    # def publish_heartbeat(self):
+    #     '''Send heartbeat message every HEARTBEAT_PERIOD seconds.
+    #
+    #     HEARTBEAT_PERIOD is set and can be adjusted in the settings module.
+    #     '''
+    #     topic = "/agent/ui/dashboard"
+    #     now = datetime.datetime.utcnow().isoformat(' ') + 'Z'
+    #     headers = {
+    #         'AgentID': self._agent_id,
+    #         headers_mod.CONTENT_TYPE: headers_mod.CONTENT_TYPE.PLAIN_TEXT,
+    #         headers_mod.DATE: now,
+    #         'data_source': "powermeterApp",
+    #     }
+    #     message = json.dumps({"monthly_electricity_bill": self.monthly_electricity_bill,
+    #                           "last_month_bill": self.last_month_bill,
+    #                           "last_month_bill_compare": self.last_month_bill_compare,
+    #                           "daily_electricity_bill": self.daily_electricity_bill,
+    #                           "last_day_bill": self.last_day_bill,
+    #                           "last_day_bill_compare": self.last_day_bill_compare,
+    #                           "daily_bill_AC": self.daily_bill_AC,
+    #                           "daily_bill_light": self.daily_bill_light,
+    #                           "daily_bill_plug": self.daily_bill_plug,
+    #                           "daily_bill_EV": self.daily_bill_EV,
+    #                           "daily_bill_AC_compare_percent": self.daily_bill_AC_compare_percent,
+    #                           "daily_bill_light_compare_percent": self.daily_bill_light_compare_percent,
+    #                           "daily_bill_plug_compare_percent": self.daily_bill_plug_compare_percent,
+    #                           "daily_bill_EV_compare_percent": self.daily_bill_EV_compare_percent,
+    #                           "monthly_bill_AC": self.monthly_bill_AC,
+    #                           "monthly_bill_light": self.monthly_bill_light,
+    #                           "monthly_bill_plug": self.monthly_bill_plug,
+    #                           "monthly_bill_EV": self.daily_bill_EV, #need to fix it
+    #                           "monthly_bill_AC_compare_percent": self.monthly_bill_AC_compare_percent,
+    #                           "monthly_bill_light_compare_percent": self.monthly_bill_light_compare_percent,
+    #                           "monthly_bill_plug_compare_percent": self.monthly_bill_plug_compare_percent,
+    #                           "monthly_bill_EV_compare_percent": self.monthly_bill_EV_compare_percent,
+    #                           "daily_energy_usage": self.daily_energy_usage,
+    #                           "last_day_energy_usage": self.last_day_energy_usage,
+    #                           "monthly_energy_usage": self.monthly_energy_usage,
+    #                           "last_month_energy_usage": self.last_month_energy_usage,
+    #                           "max_monthly_energy_usage": self.max_monthly_energy_usage,
+    #                           "max_energy_usage_month": self.max_energy_usage_month,
+    #                           "max_energy_usage_year": self.max_energy_usage_year,
+    #                           "netzero_condition": self.netzero_condition,
+    #                           "netzero_onsite_generation": self.netzero_onsite_generation,
+    #                           "netzero_energy_consumption": self.netzero_energy_consumption})
+    #     self.publish(topic, headers, message)
+    #     print ("{} published topic: {}, message: {}").format(self._agent_id, topic, message)
 
 
 def main(argv=sys.argv):
