9ff07ff3284995172057d4c45195fb28df36cc0c
TPponmat <teerapong.pon@gmail.com>
2017-07-27 16:11:19 +0700

    add Netatmo agent

--- settings.py
+++ settings.py
@@ -118,7 +118,7 @@ DATABASES = {
         'TABLE_temp_failure_time': 'temp_failure_time',
         'TABLE_priority': 'priority',
         'TABLE_seen_notifications_counter': 'seen_notifications_counter',
-        'TABLE_powermeter': 'powermeter',
+        'TABLE_powermeter': 'netatmo',
         'TABLE_AC': 'ac',
         'TABLE_Refrigerator': 'Refrigerator',
         'TABLE_MultiSensor': 'MultiSensor',
@@ -145,6 +145,7 @@ DATABASES = {
         'TABLE_pvrealtime_hourly': 'pvrealtime_hourly',
         'TABLE_CreativePowerMeter': 'CreativePowerMeter',
         'TABLE_ACDaikin': 'ACDaikin',
+        'TABLE_Netatmo': 'Netatmo',
     }
 }
 
