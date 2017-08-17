59c80388786405bd52a23031c3e4b92a886c6eb9
kwarodom <kwarodom@vt.edu>
2017-08-16 21:41:50 -0700

    change BR to parse data from classAPI_PowerMeter

--- DeviceAPI/classAPI/classAPI_PowerMeter.py
+++ DeviceAPI/classAPI/classAPI_PowerMeter.py
@@ -142,7 +142,7 @@ class API:
         self.set_variable('time', ts)
 
         # Grid
-        z1 = ((x['report'].split('<BR>'))[4]).split(',')
+        z1 = ((x['report'].split('<BR>'))[10]).split(',')
         grid_current = float(((z1[0].split('='))[1]).split(' ')[0])
         grid_activePower = float(((z1[1].split('='))[1]).split(' ')[0])
         grid_reactivePower = float(((z1[2].split('='))[1]).split(' ')[0])
@@ -164,7 +164,7 @@ class API:
         # Load
         #z3 = ((x['report'].split('<BR>'))[10]).split(',')
         #z3 change with new configuration
-        z3 = ((x['report'].split('<BR>'))[7]).split(',')
+        z3 = ((x['report'].split('<BR>'))[4]).split(',')
         load_current = float(((z3[0].split('='))[1]).split(' ')[0])
         load_activePower = float(((z3[1].split('='))[1]).split(' ')[0])
         load_reactivePower = float(((z3[2].split('='))[1]).split(' ')[0])
