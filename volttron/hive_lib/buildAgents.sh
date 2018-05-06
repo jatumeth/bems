832a1f759bc10e23058ae3a0b58c2927a7d1890e
TPponmat <nott_showba@hotmail.com>
2018-05-06 20:18:59 +0700

    add new device info and runbulld agend file

--- volttron/hive_lib/buildAgents.sh
+++ volttron/hive_lib/buildAgents.sh
@@ -10,7 +10,7 @@
 #__created__ = "2018-04-4 12:04:50"
 #__lastUpdated__ = "2018-04-4 11:23:33"
 
-cd ~/workspace/hive_os/
+cd ~/workspace/hive_os/volttron/
 . env/bin/activate
 volttron -vv 2>&1 | tee ~/workspace/hive_os/volttron/log/volttron.log &
 
@@ -23,19 +23,16 @@ cd ~/workspace/hive_os/
 
 volttron-pkg package Agents/LightingAgent
 # Set the agent's configuration file
-volttron-pkg configure ~/.volttron/packaged/lightingagent-0.1-py2-none-any.whl ~/workspace/hive_os/volttron/Agents/LightingAgent/1KR221445K1200138.config.json
+volttron-pkg configure ~/.volttron/packaged/lightingagent-0.1-py2-none-any.whl ~/workspace/hive_os/volttron/Agents/LightingAgent/02ORV0017886.config.json
 
 # Install the agent (volttron must be running):
 volttron-ctl install ~/.volttron/packaged/lightingagent-0.1-py2-none-any.whl --tag lighting
 
-volttron-ctl start --tag testagent
 
 #Fibaro Agent
 
-/home/peahive/
-
 volttron-pkg package Agents/FibaroAgent
-volttron-pkg configure ~/.volttron/packaged/fibaroagent-0.1-py2-none-any.whl ~/workspace/hive_os/volttron/Agents/FibaroAgent/8MS221445K1200138.config.json
+volttron-pkg configure ~/.volttron/packaged/fibaroagent-0.1-py2-none-any.whl ~/workspace/hive_os/volttron/Agents/FibaroAgent/20FIB1234568.config.json
 
 volttron-ctl install ~/.volttron/packaged/fibaroagent-0.1-py2-none-any.whl --tag fibaro
 
@@ -43,7 +40,7 @@ volttron-ctl install ~/.volttron/packaged/fibaroagent-0.1-py2-none-any.whl --tag
 
 volttron-pkg package Agents/NetatmoAgent
 
-volttron-pkg configure ~/.volttron/packaged/netatmoagent-0.1-py2-none-any.whl ~/workspace/hive_os/volttron/Agents/NetatmoAgent/1WE221445K1200138.config.json
+volttron-pkg configure ~/.volttron/packaged/netatmoagent-0.1-py2-none-any.whl ~/workspace/hive_os/volttron/Agents/NetatmoAgent/12NET1234510.config.json
 
 
 volttron-ctl install ~/.volttron/packaged/netatmoagent-0.1-py2-none-any.whl --tag netatmo
@@ -55,32 +52,35 @@ volttron-pkg package Agents/WeatherAgent
 volttron-pkg configure ~/.volttron/packaged/weatheragent-0.1-py2-none-any.whl ~/workspace/hive_os/volttron/Agents/WeatherAgent/1WE221445K1200138.config.json
 
 
-volttron-ctl install ~/.volttron/packaged/netatmoagent-0.1-py2-none-any.whl --tag weather
+volttron-ctl install ~/.volttron/packaged/weatheragent-0.1-py2-none-any.whl --tag weather
 
 
 #Powermeter Agent
 
-volttron-pkg package Agents/PowermeterAgent
+volttron-pkg package Agents/PowerMeterAgent
 
-volttorn-pkg configure ~/.volttron/packaged/lightingagent-0.1-py2-none-any.whl ~/workspace/hive_os/volttron/Agents/PowerMeterAgent/PMT1445K1200138.config.json
+volttron-pkg configure ~/.volttron/packaged/powermeteragent-0.1-py2-none-any.whl ~/workspace/hive_os/volttron/Agents/PowerMeterAgent/05CRE0061217.config.json
 
-volttron-ctl install ~/.volttron/packaged/netatmoagent-0.1-py2-none-any.whl --tag powermeter
+
+volttron-ctl install ~/.volttron/packaged/powermeteragent-0.1-py2-none-any.whl --tag powermeter
 
 #Yale Agent
 
 volttron-pkg package Agents/YaleAgent
 
-volttorn-pkg configure ~/.volttron/packaged/lightingagent-0.1-py2-none-any.whl ~/workspace/hive_os/volttron/Agents/YaleAgent/18DOR1445K1200138.config.json
+volttron-pkg configure ~/.volttron/packaged/yaleagent-0.1-py2-none-any.whl ~/workspace/hive_os/volttron/Agents/YaleAgent/09YAL1234567.config.json
 
-volttron-ctl install ~/.volttron/packaged/netatmoagent-0.1-py2-none-any.whl --tag yale
+
+volttron-ctl install ~/.volttron/packaged/yaleagent-0.1-py2-none-any.whl --tag yale
 
 #OpenClose Agent
 
 volttron-pkg package Agents/OpenCloseAgent
 
-volttorn-pkg configure ~/.volttron/packaged/lightingagent-0.1-py2-none-any.whl ~/workspace/hive_os/volttron/Agents/OpenCloseAgent/18DOR1445K1200138.config.json
+volttron-pkg configure ~/.volttron/packaged/opencloseagent-0.1-py2-none-any.whl ~/workspace/hive_os/volttron/Agents/OpenCloseAgent/18ORV0832132.config.json
 
-volttron-ctl install ~/.volttron/packaged/netatmoagent-0.1-py2-none-any.whl --tag openclose
+
+volttron-ctl install ~/.volttron/packaged/opencloseagent-0.1-py2-none-any.whl --tag openclose
 
 
 
