4b088f26c321522d2d0911566c0691fa5edfe25a
Soulweed <nontachai.y@gmail.com>
2018-05-25 14:59:40 +0700

    Fix

--- volttron/Agents/AutomationManagerAgent/automation_manager/agent.py
+++ volttron/Agents/AutomationManagerAgent/automation_manager/agent.py
@@ -107,7 +107,7 @@ def automation_manager_agent(config_path, **kwargs):
         def onstart(self, sender, **kwargs):
             print("On Start Event")
             # self.build_automation_agent(55)
-            
+
         @PubSub.subscribe('pubsub', topic_automation_create)  # On Automation create
         def match_topic_create(self, peer, sender, bus,  topic, headers, message):
             print("Match Topic Create Automation")
